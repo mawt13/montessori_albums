d649378737ec798fa2062367c7bba1e8fa5fc15b
Matthew Thompson <mkthompson@rescocompany.com>
2023-08-24 11:22:31 -0500

    move default dark mode to light mode, also deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TMP_DIR="/Volumes/RAMDisk"
+WEB_SERVER="mawt@10.0.0.3"
+
+cd ~/home_dev/montessori_albums/
+
+# remove docker image based on name of project
+docker rmi $(docker images 'mawt/montessori_albums:latest' -a -q)
+
+# build docker image
+docker build -t mawt/montessori_albums .
+
+# save docker image to tar file
+docker save -o $TMP_DIR/montessori_albums.tar mawt/montessori_albums:latest
+
+# copy tar file to server
+scp /Volumes/RAMDisk/montessori_albums.tar $WEB_SERVER:/ram/
+
+### server cmds
+echo 'Executing server cmds...'
+# cd ~/docker/montessori_albums/
+ssh $WEB_SERVER 'cd docker/montessori_albums; docker-compose down'
+ssh $WEB_SERVER 'docker rmi $(docker images "mawt/montessori_albums:latest" -a -q)'
+ssh $WEB_SERVER 'docker load -i /ram/montessori_albums.tar'
+ssh $WEB_SERVER 'cd docker/montessori_albums; docker-compose up -d'
+ssh $WEB_SERVER 'rm /ram/montessori_albums.tar'
+
+### finished server cmd
+echo 'Finished server cmds...'
+
+# remove temp files
+rm $TMP_DIR/montessori_albums.tar
+echo 'done'
